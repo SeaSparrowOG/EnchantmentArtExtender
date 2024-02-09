34e6e2e4b524c9aab4e3a0fd0b38744ae55d7e7d
SeaSparrow <contactMeOnNexus@gmail.com>
2024-02-10 01:06:16 +0200

    Light effect attempt 1

--- src/actorEventManager.cpp
+++ src/actorEventManager.cpp
@@ -29,15 +29,45 @@ namespace {
 			ActorEvents::AnimationEventListener::GetSingleton()->RegisterActor(a_actor);
 		}
 
+		auto* magicLight = Cache::StoredData::GetSingleton()->lightObject;
+		auto* magicSpell = Cache::StoredData::GetSingleton()->lightSpell;
+		float red = magicLight ? magicLight->emittanceColor.red : 255.0f;
+		float green = magicLight ? magicLight->emittanceColor.green : 255.0f;
+		float blue = magicLight ? magicLight->emittanceColor.blue : 255.0f;
+
 		if (handsUp && rightWeapon) {
 			auto* enchantment = rightWeapon->formEnchanting;
 			if (!enchantment) enchantment = rightData->GetEnchantment();
 
 			auto swaps = Cache::StoredData::GetSingleton()->GetMatchingSwaps(rightWeapon, enchantment);
 			for (auto& swap : swaps) {
-				auto* rightAbility = swap.rightAbility;
-				if (!SpellVectorContainsElement(rightAbility, &abilities))
-					abilities.push_back(rightAbility);
+				auto* spell = swap.rightAbility;
+				if (!SpellVectorContainsElement(spell, &abilities)) {
+					if (Cache::StoredData::GetSingleton()->GetShouldAddLight()) {
+						for (auto* effect : spell->effects) {
+							auto* baseEffect = effect->baseEffect;
+							bool valid = baseEffect ? baseEffect->GetArchetype() == RE::EffectSetting::Archetype::kLight : false;
+							if (!valid) continue;
+
+							auto* effectForm = baseEffect->data.associatedForm;
+							auto* associatedLight = effectForm ? effectForm->As<RE::TESObjectLIGH>() : nullptr;
+							if (!associatedLight) continue;
+
+							auto color = associatedLight->data.color;
+							uint8_t currentRed = color.red;
+							uint8_t currentGreen = color.green;
+							uint8_t currentBlue = color.blue;
+
+							if (currentRed < red)
+								red = currentRed;
+							if (currentGreen < green)
+								green = currentGreen;
+							if (currentBlue < blue)
+								blue = currentBlue;
+						}
+					}
+					abilities.push_back(spell);
+				}
 			}
 		}
 
@@ -47,9 +77,33 @@ namespace {
 
 			auto swaps = Cache::StoredData::GetSingleton()->GetMatchingSwaps(leftWeapon, enchantment);
 			for (auto& swap : swaps) {
-				auto* leftAbility = swap.leftAbility;
-				if (!SpellVectorContainsElement(leftAbility, &abilities))
-					abilities.push_back(leftAbility);
+				auto* spell = swap.leftAbility;
+				if (!SpellVectorContainsElement(spell, &abilities)) {
+					if (Cache::StoredData::GetSingleton()->GetShouldAddLight()) {
+						for (auto* effect : spell->effects) {
+							auto* baseEffect = effect->baseEffect;
+							bool valid = baseEffect ? baseEffect->GetArchetype() == RE::EffectSetting::Archetype::kLight : false;
+							if (!valid) continue;
+
+							auto* effectForm = baseEffect->data.associatedForm;
+							auto* associatedLight = effectForm ? effectForm->As<RE::TESObjectLIGH>() : nullptr;
+							if (!associatedLight) continue;
+
+							auto color = associatedLight->data.color;
+							uint8_t currentRed = color.red;
+							uint8_t currentGreen = color.green;
+							uint8_t currentBlue = color.blue;
+
+							if (currentRed < red)
+								red = currentRed;
+							if (currentGreen < green)
+								green = currentGreen;
+							if (currentBlue < blue)
+								blue = currentBlue;
+						}
+					}
+					abilities.push_back(spell);
+				}
 			}
 		}
 
@@ -62,6 +116,16 @@ namespace {
 		for (auto* spell : allAbilities) {
 			if (!SpellVectorContainsElement(spell, &abilities) && a_actor->HasSpell(spell))
 				a_actor->RemoveSpell(spell);
+		}
+
+		if (magicSpell && a_actor->HasSpell(magicSpell))
+			a_actor->RemoveSpell(magicSpell);
+
+		if (Cache::StoredData::GetSingleton()->GetShouldAddLight() && !a_actor->HasSpell(magicSpell)) {
+			magicLight->data.color.red = red;
+			magicLight->data.color.green = green;
+			magicLight->data.color.blue = blue;
+			a_actor->AddSpell(magicSpell);
 		}
 	}
 }
