e56a5ff2e86fbeda570cf1e7adcec573ee05713a
SeaSparrow <SeaSparrowNexus@outlook.com>
2024-10-17 10:53:20 +0300

    Cut cache building time down by 50%

--- src/settingsReader.cpp
+++ src/settingsReader.cpp
@@ -68,8 +68,8 @@ namespace Settings {
 		std::string requiredFieldsAtLeastOne[] = {"EnchantmentKeywords", "Enchantment"};
 		bool foundAllFields = true;
 
-		for (auto field : requiredFields) {
-			auto found = a_JSON[field];
+		for (const auto& field : requiredFields) {
+			const auto& found = a_JSON[field];
 			if (!found) {
 				foundAllFields = false;
 				response.valid = false;
@@ -80,8 +80,8 @@ namespace Settings {
 		//New in version code 2: Enchantment Keywords and Enchantments. Need at least one.
 		bool foundOne = false;
 
-		for (auto field : requiredFieldsAtLeastOne) {
-			auto found = a_JSON[field];
+		for (const auto& field : requiredFieldsAtLeastOne) {
+			const auto& found = a_JSON[field];
 			if (found)
 				foundOne = true;
 		}
@@ -92,14 +92,14 @@ namespace Settings {
 		}
 
 		if (a_JSON["Enchantment"]) {
-			auto allFields = a_JSON["Enchantment"];
+			const auto& allFields = a_JSON["Enchantment"];
 			if (allFields.size() != 2) {
 				response.valid = false;
 				response.foundGarbage.push_back("Enchantment");
 			}
 			else {
-				auto idRaw = a_JSON["Enchantment"]["ID"];
-				auto sourceRaw = a_JSON["Enchantment"]["Source"];
+				const auto& idRaw = a_JSON["Enchantment"]["ID"];
+				const auto& sourceRaw = a_JSON["Enchantment"]["Source"];
 				if (!(idRaw && sourceRaw)) {
 					response.valid = false;
 					response.foundGarbage.push_back("Enchantment");
@@ -115,10 +115,10 @@ namespace Settings {
 
 		//Check 3: Check for garbage.
 		std::string expectedFields[] = { "MinimumVersion", "EnchantmentKeywords", "Enchantment", "ArtSource", "SwapData", "Exclusive", "$schema" };
-		auto allFields = a_JSON.getMemberNames();
-		for (auto field : allFields) {
+		const auto& allFields = a_JSON.getMemberNames();
+		for (const auto& field : allFields) {
 			bool fieldIsValid = false;
-			for (auto expectedField : expectedFields) {
+			for (const auto& expectedField : expectedFields) {
 				if (field == expectedField) {
 					fieldIsValid = true;
 				}
@@ -136,32 +136,32 @@ namespace Settings {
 		std::string expectedBool[] = { "Exclusive" };
 		std::string expectedTopLevelObject[] = { "Enchantment" };
 
-		for (auto entry : expectedString) {
-			auto field = a_JSON[entry];
+		for (const auto& entry : expectedString) {
+			const auto& field = a_JSON[entry];
 			if (!field) continue;
 			if (field.isString()) continue;
 			response.valid = false;
 			response.expectedString.push_back(entry);
 		}
 
-		for (auto entry : expectedList) {
-			auto field = a_JSON[entry];
+		for (const auto& entry : expectedList) {
+			const auto& field = a_JSON[entry];
 			if (!field) continue;
 			if (field.isArray()) continue;
 			response.valid = false;
 			response.expectedString.push_back(entry);
 		}
 
-		for (auto entry : expectedBool) {
-			auto field = a_JSON[entry];
+		for (const auto& entry : expectedBool) {
+			const auto& field = a_JSON[entry];
 			if (!field) continue;
 			if (field.isBool()) continue;
 			response.valid = false;
 			response.expectedBool.push_back(entry);
 		}
 
-		for (auto entry : expectedTopLevelObject) {
-			auto field = a_JSON[entry];
+		for (const auto& entry : expectedTopLevelObject) {
+			const auto& field = a_JSON[entry];
 			if (!field) continue;
 			if (field.isObject()) continue;
 			response.valid = false;
@@ -170,29 +170,29 @@ namespace Settings {
 
 		//Check for garbage and missing masters in SwapData.
 		if (a_JSON["SwapData"] && a_JSON["SwapData"].isArray()) {
-			for (auto data : a_JSON["SwapData"]) {
+			for (const auto& data : a_JSON["SwapData"]) {
 				if (!data.isObject()) {
 					response.valid = false;
 					response.expectedObject.push_back("thing");
 					continue;
 				}
 
-				auto allDataNames = data.getMemberNames();
+				const auto& allDataNames = data.getMemberNames();
 				if (allDataNames.size() != 1) {
 					response.valid = false;
 					response.badSwapData.push_back("Non-object in SwapData.");
 					continue;
 				}
 
-				auto swapName = allDataNames.at(0);
-				auto swapInformation = data[swapName];
+				const auto& swapName = allDataNames.at(0);
+				const auto& swapInformation = data[swapName];
 				if (!swapInformation.isObject()) {
 					response.valid = false;
 					response.badSwapData.push_back("Expected " + swapName + " to be an object, but isn't.");
 					continue;
 				}
 
-				auto informationNames = swapInformation.getMemberNames();
+				const auto& informationNames = swapInformation.getMemberNames();
 				int matches = 0;
 				for (auto name : expectedInformationNames) {
 					for (std::string field : informationNames) {
@@ -209,8 +209,8 @@ namespace Settings {
 				}
 
 				matches = 0;
-				for (auto name : informationNames) {
-					for (auto field : expectedInformationAtLeastOne) {
+				for (const auto& name : informationNames) {
+					for (const auto& field : expectedInformationAtLeastOne) {
 						if (name == std::string(field)) {
 							++matches;
 						}
